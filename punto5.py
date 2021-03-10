d46498675768f7dc2dea6419601f77a36d8c87a6
Juan Diego <jdidelarc@gmail.com>
2021-03-10 11:58:23 -0500

    punto 5 añadido

--- punto5.py
+++ punto5.py
@@ -1,14 +1,14 @@
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator
-
+import os
 
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
+import sympy as sym
 
-# Seleccionar lo que se desea hacer, graficar demora unos 5 minutos
-AREA = False
-GRAPH = True
+# Seleccionar lo que se desea hacer
+# Calcular el área tarda alrededor de 12 min
+
+VOLUME = True
+GRAPH = False
 
 
 def load_dataset():
@@ -16,66 +16,116 @@ def load_dataset():
     quads = np.loadtxt('mallas/valle_aburra-quads.quad', dtype=np.int64)
     return pts, quads
 
+def output_image(pts):
+    X = pts[:, 0]
+    Y = pts[:, 1]
+    Z = pts[:, 2]
 
-def output_image(pts, quads):
+    X.shape = 272, 173
+    Y.shape = 272, 173
+    Z.shape = 272, 173
+
+    graph = plt.contourf(X, Y, Z, cmap='viridis')
+    plt.contour(X, Y, Z, colors="black")
+    plt.axis("image")
+    plt.colorbar(graph)
+    plt.savefig(os.path.join('outputs', 'punto5-contour.png'), dpi=500)
+
+    fig = plt.figure()
+
+    ax = fig.gca(projection='3d')
+    ax.plot_surface(X, Y, Z, cmap="viridis")
+
+    fig.savefig(os.path.join('outputs', 'punto5-surface.png'), dpi=500)
+
+
+# Definición de las funciones que se explica más claramente en el documento
+def translate_to_origin(pts):
+    '''
+    Translada puntos del elemento de 0.045 x 0.045 para que queden centrados sobre el eje xy
+    '''
+
+    points = pts
+
+    _min_x = points[:, 0].min()
+    _min_y = points[:, 1].min()
+
+    points[:, 0] = points[:, 0] - _min_x - 0.045
+    points[:, 1] = points[:, 1] - _min_y - 0.045
+
+    return points
+
+def interpolate(points):
+    '''
+    Retorna una interpolación de Lagrange para los 4 puntos del elemento dado
+    '''
+
+    x, y = sym.symbols('x y')
+
+    NA = (1/4)*((1/0.045)**2)*(0.045 - x)*(0.045 + y)
+    NB = (1/4)*((1/0.045)**2)*(0.045 - x)*(0.045 - y)
+    NC = (1/4)*((1/0.045)**2)*(0.045 + x)*(0.045 - y)
+    ND = (1/4)*((1/0.045)**2)*(0.045 + x)*(0.045 + y)
+
+    return NA*points[0, 2] + NB*points[1, 2] + NC*points[2, 2] + ND*points[3, 2]
+
+def gaussian_quad(function: sym.core.add.Add):
+    '''Cuadratura gaussiana de n = 3 dada una función. Límites entre [-0.045, 0.045]'''
+
+    x, y = sym.symbols('x y')
+
+    _lim = [-0.045, 0.045]
+    _p_and_w = [[-np.sqrt(3/5), 5/9],[0, 8/9], [np.sqrt(3/5), 5/9]]
+
+    _coef = (_lim[1] -_lim[0]) / 2
+
+    var = lambda xi: (_coef*xi) + ((_lim[1] + _lim[0]) / 2)
+    
+    summ = 0
+
+
+    for _y in _p_and_w:
+        res = 0
+        
+        for _x in _p_and_w:
+            
+            res += function.subs({x: var(_x[0])})*_x[1]
+        
+        summ += res.subs({y: var(_y[0])})*_y[1]
+        
+    return _coef**2*summ
+
+def compute_volume(pts, quads):
+
+    # Se definen los puntos normalizados, restando el z mínimo
+    n_points = pts.copy()
+    n_points[:, 2] = n_points[:, 2] - n_points[:, 2].min()
+
+    total_vol = 0
+
+    for idx, quad in enumerate(quads):
+
+        elem = n_points[quad]
+        t_points = translate_to_origin(elem)
+        i_funct = interpolate(t_points)
+
+        vol = gaussian_quad(i_funct)
+
+        print(f'Volumen del elemento {idx}: {vol:.4f} km^3')
+
+        total_vol += vol
+    
+    print()
+    print(f'Volumen total: {total_vol} km^3')
 
     
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-
-    # Make data.
-    X = np.arange(-5, 5, 0.25)
-    Y = np.arange(-5, 5, 0.25)
-    X, Y = np.meshgrid(X, Y)
-    R = np.sqrt(X**2 + Y**2)
-    Z = np.sin(R)
-
-    # Plot the surface.
-    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                        linewidth=0, antialiased=False)
-
-    # Customize the z axis.
-    ax.set_zlim(-1.01, 1.01)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    # A StrMethodFormatter is used automatically
-    ax.zaxis.set_major_formatter('{x:.02f}')
-
-    # Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-
-
-def compute_area(pts, quads):
-
-    z_min = np.amin(pts[:, 2])  # Valor mínimo de z para calcular el area
-
-    n_pts = pts - z_min         # Normalizar los puntos respecto al valor mínimo
-
-    area = 0
-
-    for verts in quads:  # Asumiendo que todos los elementos son paralelogramos
-
-        '''
-        print(verts)
-        print(pts[verts])
-        '''
-
-        _w = np.abs(pts[verts[0], 0] - pts[verts[2], 0])
-        _l = np.abs(pts[verts[0], 1] - pts[verts[1], 1])
-        _h = (n_pts[verts][:, 2]).mean()
-
-        area += _w * _h*_l
-
-    print(f'Volumen: {area:.4f} km^3')
-
-
 def main():
     pts, quads = load_dataset()
 
     if GRAPH:
-        output_image(pts, quads)
-    elif AREA:
-        compute_area(pts, quads)
-    else:
-        print('No options selected')
+        output_image(pts)
+    if VOLUME:
+        compute_volume(pts, quads)
 
 
 if __name__ == '__main__':
